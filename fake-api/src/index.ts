9d409e7c2a52faff9d374afa9e170bbdee93b261
Sebastian Blasetti <sblasetti@gmail.com>
2018-12-04 00:54:41 -0300

    Added JSON response mockups for user and events endpoint in the fake-api

--- fake-api/src/index.ts
+++ fake-api/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import * as userJson from './data/user.json';
+import * as eventsJson from './data/events.json';
 
 const app: express.Application = express();
 const port: number = 3005;
@@ -13,10 +15,18 @@ app.use(function (req: express.Request, res: express.Response, next: express.Nex
 
 // Fake method to retrieve user information
 app.get('/rest/api/user', (req: express.Request, res: express.Response) => {
-    console.log('return fake user data');
-    res.status(200).send({
-        username: req.query.username
-    });
+    console.log('return fake user data (user must be "test")');
+    if (req.query.username === "test") {
+        res.status(200).send(userJson);
+    } else {
+        res.status(404).send({});
+    }
+});
+
+// Fake method to retrieve events
+app.get('/rest/calendar-services/1.0/calendar/events.json', (req: express.Request, res: express.Response) => {
+    console.log('return fake events data');
+    res.status(200).send(eventsJson);
 });
 
 // Serve the app
