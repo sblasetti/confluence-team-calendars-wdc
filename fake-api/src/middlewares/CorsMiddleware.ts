2336b31107c800ae0bce271b2d2cc08773ea77e1
Sebastian Blasetti <sblasetti@gmail.com>
2018-12-04 01:31:04 -0300

    Configured CORS headers to allow the connector as origin (temporary)

--- fake-api/src/middlewares/CorsMiddleware.ts
+++ fake-api/src/middlewares/CorsMiddleware.ts
@@ -3,19 +3,18 @@ import { NextFunction, Response, Request } from "express";
 export const CorsMiddleware = () => {
     return function CorsMiddleware(req: Request, res: Response, next: NextFunction) {
         // Website you wish to allow to connect
-        // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8008');
+        res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3003'); // temporary allowing the connector as origin
 
         // Request methods you wish to allow
-        // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
         // Request headers you wish to allow
-        // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Headers', 'Authorization,Content-Type');
 
         // Set to true if you need the website to include cookies in the requests sent
         // to the API (e.g. in case you use sessions)
         // res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-        // Pass to next layer of middleware
         next();
     };
 }
